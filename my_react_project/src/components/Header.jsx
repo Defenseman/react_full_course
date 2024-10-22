636cfe5c3834a8b1b2c43517593ef9fe1341df63
Defenseman <worknesterov@yandex.kz>
2024-10-22 09:37:43 +0500

    react tutorial

---
+++ my_react_project/src/components/Header.jsx
@@ -0,0 +1,19 @@
+
+function Header() {
+    return(
+        <header className="Header">
+            <h1>My website</h1>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>FAQ</li>
+                </ul>
+            </nav>
+            <hr></hr>
+        </header>
+    );
+}
+
+export default Header;
